c6a39b7ba499ac504163aab3002ceaaa342603f1
Marija Mihajlov <mojemail@gmail.com>
2021-05-04 19:14:06 +0200

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { motion } from 'framer-motion'
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+
 
 const Home = () => {
   return (
     <motion.div className={styles.homeContainer}
     initial={{opacity: 0}}
     animate={{opacity:1, fontSize:"11em"}}
-    transition={{delay: 0.2, duration:2}}
+    transition={{delay: 0.1, duration:1.3}}
     >
 
 <span className={styles.fit}>M</span>i<span className={styles.fit}>S</span>TER
