ca8d17c39e83c47a3da5272d5680d65baeedc0d1
Marija Mihajlov <mojemail@gmail.com>
2021-05-18 00:21:59 +0200

    Responsive commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
+import useWindowSize from "../hooks/useWindowSize";
 
 const Home = () => {
   return (
@@ -11,11 +12,11 @@ const Home = () => {
       <motion.div
         className={styles.homeContainer}
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, fontSize: "11em" }}
+        animate={{ opacity: 1, fontSize: "10vw" }}
         transition={{ delay: 0.1, duration: 1.1 }}
       >
-        <span className={styles.fit}>M</span>i
-        <span className={styles.fit}>S</span>TER
+        <span className={styles.mis}>M</span>i
+        <span className={styles.mis}>S</span>TER
         <motion.span animate={{ y: 100 }}>fit</motion.span>
       </motion.div>
     </>
@@ -23,3 +24,30 @@ const Home = () => {
 };
 
 export default Home;
+
+// const Home = () => {
+//   // const size = useWindowSize();
+//   // console.log(size);
+//   return (
+//     <>
+//       <Head>
+//         <title>Home</title>
+//       </Head>
+//       <motion.div
+//         className={styles.homeContainer}
+//         initial={{ opacity: 0 }}
+//         animate={{
+//           opacity: 1,
+//           fontSize: "12vw",
+//         }}
+//         transition={{ delay: 0.1, duration: 1.1 }}
+//       >
+//         <span className={styles.fit}>M</span>i
+//         <span className={styles.fit}>S</span>TER
+//         <motion.span animate={{ y: 100 }}>fit</motion.span>
+//       </motion.div>
+//     </>
+//   );
+// };
+
+// export default Home;
